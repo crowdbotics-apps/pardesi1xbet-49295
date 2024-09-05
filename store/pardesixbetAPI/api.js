28284960b08fad8190baed33b35703c4545886c9
Crowdbotics <engineering@crowdbotics.com>
2024-09-05 11:04:02 +0000

    Update store.

--- store/pardesixbetAPI/api.js
+++ store/pardesixbetAPI/api.js
@@ -14,6 +14,30 @@ function api_v1_login_create(payload) {
 function api_v1_signup_create(payload) {
   return pardesixbetAPI.post(`/api/v1/signup/`, payload)
 }
+function modules_terms_and_conditions_list(payload) {
+  return pardesixbetAPI.get(`/modules/terms-and-conditions/`)
+}
+function modules_terms_and_conditions_create(payload) {
+  return pardesixbetAPI.post(`/modules/terms-and-conditions/`, payload)
+}
+function modules_terms_and_conditions_retrieve(payload) {
+  return pardesixbetAPI.get(`/modules/terms-and-conditions/${payload.id}/`)
+}
+function modules_terms_and_conditions_update(payload) {
+  return pardesixbetAPI.put(
+    `/modules/terms-and-conditions/${payload.id}/`,
+    payload
+  )
+}
+function modules_terms_and_conditions_partial_update(payload) {
+  return pardesixbetAPI.patch(
+    `/modules/terms-and-conditions/${payload.id}/`,
+    payload
+  )
+}
+function modules_terms_and_conditions_destroy(payload) {
+  return pardesixbetAPI.delete(`/modules/terms-and-conditions/${payload.id}/`)
+}
 function rest_auth_login_create(payload) {
   return pardesixbetAPI.post(`/rest-auth/login/`, payload)
 }
@@ -51,6 +75,12 @@ export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
+  modules_terms_and_conditions_list,
+  modules_terms_and_conditions_create,
+  modules_terms_and_conditions_retrieve,
+  modules_terms_and_conditions_update,
+  modules_terms_and_conditions_partial_update,
+  modules_terms_and_conditions_destroy,
   rest_auth_login_create,
   rest_auth_logout_create,
   rest_auth_password_change_create,
