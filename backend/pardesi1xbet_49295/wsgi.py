28938cd6dfb48ad06069bee2304e6d8f4dc0c1d1
crowdbotics-apps <team@crowdbotics.com>
2024-09-05 09:43:31 +0000

    Update models

--- backend/pardesi1xbet_49295/wsgi.py
+++ backend/pardesi1xbet_49295/wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "pardesi1xbet_49295.settings"
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pardesi1xbet_49295.settings")
 
 application = get_wsgi_application()
